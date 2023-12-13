6cf28e1d5aa90c349f97076d1e4630f05f73632d
fendahaha <troneyf001@gmail.com>
2023-12-13 16:42:53 +0800

    asd

--- src/main/java/com/example/file_server/controller/ControllerAdvice.java
+++ src/main/java/com/example/file_server/controller/ControllerAdvice.java
@@ -5,9 +5,12 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -35,4 +38,50 @@ public class ControllerAdvice {
         });
         return ResponseUtil.badRequest(hashMap);
     }
+
+    /**
+     * Validator校验普通实体失败抛出的异常
+     */
+//    @ExceptionHandler(BindException.class)
+//    public Object bindException(BindException e) {
+//        //LOGGER.info("全局异常捕获器-捕获到BindException：", e);
+//        return ResponseUtil.badRequest("");
+//    }
+
+    /**
+     * 请求方法不被允许异常
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Object httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        //LOGGER.info("全局异常捕获器-捕获到HttpRequestMethodNotSupportedException：", e);
+        return ResponseUtil.badRequest("");
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public Object httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        //LOGGER.info("全局异常捕获器-捕获到HttpRequestMethodNotSupportedException：", e);
+        return ResponseUtil.badRequest("");
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public Object noHandlerFoundException(NoHandlerFoundException e) {
+        //LOGGER.info("全局异常捕获器-捕获到NoHandlerFoundException：", e);
+        return ResponseUtil.badRequest("");
+    }
+
+//    @ExceptionHandler(AuthenticationException.class)
+//    public Object authenticationException(AuthenticationException e) {
+//        //LOGGER.info("全局异常捕获器-捕获到AuthenticationException：", e);
+//        return ResponseUtil.badRequest("");
+//    }
+
+    /**
+     * 总异常：只要出现异常，总会被这个拦截，因为所以的异常父类为：Exception
+     */
+    @ExceptionHandler(Exception.class)
+    public Object exception(Exception e) {
+        //LOGGER.info("全局异常捕获器-捕获到Exception：", e);
+        return ResponseUtil.badRequest("");
+    }
+
 }
