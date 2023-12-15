58d5368eb3d11ccacb89aad6a8adefc85a96cb33
fendahaha <troneyf001@gmail.com>
2023-12-15 15:25:23 +0800

    asd

--- src/main/java/com/example/file_server/controller/TestController.java
+++ src/main/java/com/example/file_server/controller/TestController.java
@@ -1,41 +1,109 @@
 package com.example.file_server.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.example.file_server.entity.Gift;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    public TestController(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/set")
+    public Object set(String k, String v) {
+        redisTemplate.opsForValue().set(k, v);
+        return "ok";
+    }
+
     @GetMapping("/get")
-    public Object index(String p, @CookieValue String theme) {
-        System.out.println(p);
-        System.out.println(theme);
-        return ResponseEntity.badRequest().body("bad");
+    public Object get(String k) {
+        Object o = redisTemplate.opsForValue().get(k);
+        return o;
     }
 
-    @PostMapping("/post")
-    public Object index1(String p, @RequestBody String b) {
-        System.out.println(p);
-        System.out.println(b);
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("asd", "sadadada");
-        hashMap.put("aa", true);
-        hashMap.put("age", 12);
-        return hashMap;
+    @GetMapping("/del")
+    public Object del(String k) {
+        Boolean delete = redisTemplate.delete(k);
+        return delete;
     }
 
-    @PostMapping("/post2")
-    public Object index2(String p, @RequestBody String b, String id) {
-        System.out.println(p);
-        System.out.println(b);
-        System.out.println(id);
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("asd", "sadadada");
-        hashMap.put("aa", true);
-        hashMap.put("age", 12);
-        return hashMap;
+    @GetMapping("/expire")
+    public Object expire(String k, int second) {
+        Boolean expire = redisTemplate.expire(k, Duration.ofSeconds(second));
+        return expire;
+    }
+
+    @GetMapping("/set2")
+    public Object set2() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("k1", "v1");
+        map.put("k2", 1);
+        map.put("k3", 0);
+        map.put("k4", true);
+        map.put("k5", false);
+        map.put("k6", 100.12);
+        map.put("k7", new Date());
+        redisTemplate.opsForHash().putAll("map", map);
+        return "ok";
+    }
+
+    @GetMapping("/get2")
+    public Object get2(String k) {
+        Object o = redisTemplate.opsForHash().get("map", k);
+        System.out.println(o.getClass());
+        return o;
+    }
+
+    @GetMapping("/set3")
+    public Object set3() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("k1", "v1");
+        map.put("k2", 1);
+        HashMap<String, Object> map1 = new HashMap<>();
+        map1.put("name", "fenda");
+        map1.put("k4", true);
+        map1.put("k5", false);
+        map1.put("k6", 100.12);
+        map1.put("k7", new Date());
+        map.put("people", map1);
+        redisTemplate.opsForValue().set("map3", map);
+        return "ok";
+    }
+
+    @GetMapping("/get3")
+    public Object get3() {
+        HashMap<String, Object> map = (HashMap<String, Object>) redisTemplate.opsForValue().get("map3");
+        return map;
+    }
+
+    @GetMapping("/set4")
+    public Object set4() {
+        Gift gift = new Gift();
+//        gift.setId(10);
+        gift.setGiftCreateAt(new Date());
+        gift.setGiftName("adsad");
+//        gift.setAge(0);
+        redisTemplate.opsForValue().set("gift", gift);
+        return "ok";
+    }
+
+    @GetMapping("/get4")
+    public Object get4() {
+//        Gift gift = (Gift) redisTemplate.opsForValue().get("gift");
+        Object gift = redisTemplate.opsForValue().get("gift");
+        return gift;
     }
 }
--- src/main/java/com/example/file_server/entity/User.java
+++ src/main/java/com/example/file_server/entity/User.java
@@ -24,7 +24,6 @@ public class User {
     private String userAvatar;
 
     private Integer userType;
-
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private Date createAt;
 
