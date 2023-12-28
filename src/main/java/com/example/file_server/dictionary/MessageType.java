f478c28bf8eae1df634675944f605bc9aa7be7a5
fendahaha <troneyf001@gmail.com>
2023-12-28 14:17:09 +0800

    asd

--- src/main/java/com/example/file_server/dictionary/MessageType.java
+++ src/main/java/com/example/file_server/dictionary/MessageType.java
@@ -1,9 +1,17 @@
 package com.example.file_server.dictionary;
 
+import com.example.file_server.utils.JsonUtil;
+import com.example.file_server.utils.UUIDUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Date;
+import java.util.HashMap;
+
 public enum MessageType {
     System(1),
     Gift(2),
-    chat(3);
+    chat(3),
+    Room(4);
 
     private final Integer value;
 
@@ -17,5 +25,14 @@ public enum MessageType {
 
     public boolean equals(Integer i) {
         return value.equals(i);
+    }
+
+    public HashMap<String, Object> createMessage(Object data) throws JsonProcessingException {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", UUIDUtil.generateUUID());
+        map.put("type", this.value);
+        map.put("data", JsonUtil.object2Json(data));
+        map.put("time", new Date().getTime());
+        return map;
     }
 }
